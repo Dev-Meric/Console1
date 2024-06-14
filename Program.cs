54c19d264c6589cb949aba838aa91fa7906fc6dc
Neila <neila48377036@edu.pa.senac.br>
2024-06-13 21:21:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Console1
+{
+	internal class Program{
+		static void Main(string[] args){
+			Usuario usuario = new Usuario();
+
+			usuario.nome = "CR7";
+			usuario.login = "root";
+			usuario.senha = "qwe123";
+
+			string login;
+			string senha;
+
+			do
+			{
+				Console.WriteLine("Digite seu login: ");
+				login = Console.ReadLine();
+
+				Console.WriteLine("Digite sua Senha: ");
+				senha = Console.ReadLine();
+
+				bool logado = usuario.Logar(login, senha);
+
+				if (!logado)
+				{
+					Console.WriteLine("Login ou senha incorretos, insira novamente!");
+				}
+
+			} while (!usuario.Logar(login, senha));
+
+			
+
+
+
+
+
+			//---------------------------------
+			//Console.WriteLine("Hello, " +usuario.nome);
+
+			//Console.WriteLine( usuario.Logar("root", "qwe123")  );
+
+			//string result = Console.ReadLine();
+			//Método para ler do teclado
+			//Console.WriteLine("O usuário digitou: " + result);
+		}
+	}
+}
---
+++ Usuario.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console1
+{
+	internal class Usuario{
+		public string nome;
+		public string login;
+		public string senha;
+		public int qtdLogin;
+		public float peso;
+		public bool ehSolteiro;
+		public char abrvNomeMeio;
+
+		public bool Logar(string _login, string _senha){
+			if (_login == login){
+				if (_senha == senha){ 
+					return true; 
+				}
+			}
+			
+			return false;
+		}
+	}
+}
